d0677dddb6d2ab2ce4a14a5907fdcde5c819371d
unaPalabra <122109082+unaPalabra@users.noreply.github.com>
2023-10-06 22:22:39 +0700

    1 commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,10 @@
 import InOut.ReadingXLS;
-import comparators.StudentComparator;
-import comparators.UniversityComparator;
-import enums.StudentComparatorType;
-import enums.UniversityComparatorType;
+import InOut.XlsWriter;
+import comparators.*;
+import enums.*;
 import model.*;
-import util.ComparatorUtil;
-import util.JsonUtil;
+import util.*;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -20,21 +19,19 @@ public class Main {
         universities.sort(universityComparator);
         String universitiesJson = JsonUtil.universityListToJson(universities);
 
-        // проверяем, что json создан успешно
         System.out.println(universitiesJson);
         List<University> universitiesFromJson = JsonUtil.jsonToUniversityList(universitiesJson);
 
-        // проверяем, что обратно коллекция воссоздаётся в таком же количестве элементов
         System.out.println(universities.size() == universitiesFromJson.size());
 
         System.out.println();
 
         universities.forEach(university -> {
             String universityJson = JsonUtil.universityToJson(university);
-            // проверяем, что json из отдельного элемента создан успешно
+
             System.out.println(universityJson);
             University universityFromJson = JsonUtil.jsonToUniversity(universityJson);
-            // проверяем, что обратно элемент воссоздаётся
+
             System.out.println(universityFromJson);
         });
 
@@ -46,22 +43,25 @@ public class Main {
                 ComparatorUtil.getStudentComparator(StudentComparatorType.AVG_EXAM_SCORE);
         students.sort(studentComparator);
         String studentsJson = JsonUtil.studentListToJson(students);
-        // проверяем, что json создан успешно
+
         System.out.println(studentsJson);
         List<Student> studentsFromJson = JsonUtil.jsonToStudentList(studentsJson);
-        // проверяем, что обратно коллекция воссоздаётся в таком же количестве элементов
+
         System.out.println(students.size() == studentsFromJson.size());
 
         System.out.println();
 
         students.forEach(student -> {
             String studentJson = JsonUtil.studentToJson(student);
-            // проверяем, что json из отдельного элемента создан успешно
+
             System.out.println(studentJson);
             Student studentFromJson = JsonUtil.jsonToStudent(studentJson);
-            // проверяем, что обратно элемент воссоздаётся
+
             System.out.println(studentFromJson);
         });
+
+        List<Statistics> statisticsList = StatisticsUtil.createStatistics(students, universities);
+        XlsWriter.writeXlsStatistics(statisticsList, "statistics_1.xlsx");
     }
 }
 
---
+++ src/main/java/util/StatisticsUtil.java
@@ -0,0 +1,54 @@
+package util;
+
+import enums.StudyProfile;
+import model.*;
+import java.math.*;
+import java.util.*;
+import org.apache.commons.lang3.StringUtils;
+import java.util.stream.Collectors;
+
+public class StatisticsUtil {
+
+    private StatisticsUtil() {
+    }
+
+    public static List<Statistics> createStatistics(List<Student> students,
+                                                    List<University> universities) {
+
+        List<Statistics> statisticsList = new ArrayList<>();
+
+        Set<StudyProfile> profiles = universities.stream()
+                .map(University::getMainProfile)
+                .collect(Collectors.toSet());
+
+        profiles.forEach(profile -> {
+            Statistics statistics = new Statistics();
+            statisticsList.add(statistics);
+            statistics.setProfile(profile);
+
+            List<String> profileUniversityIds = universities.stream()
+                    .filter(university -> university.getMainProfile().equals(profile))
+                    .map(University::getId)
+                    .toList();
+            statistics.setNumberOfUniversities(profileUniversityIds.size());
+            statistics.setUniversityNames(StringUtils.EMPTY);
+            universities.stream()
+                    .filter(university -> profileUniversityIds.contains(university.getId()))
+                    .map(University::getFullName)
+                    .forEach(fullNameUniversity -> statistics.setUniversityNames(
+                            statistics.getUniversityNames() + fullNameUniversity + ";"));
+            List<Student> profileStudents = students.stream()
+                    .filter(student -> profileUniversityIds.contains(student.getUniversityId()))
+                    .toList();
+            statistics.setNumberOfStudents(profileStudents.size());
+            OptionalDouble avgExamScore = profileStudents.stream()
+                    .mapToDouble(Student::getAvgExamScore)
+                    .average();
+            statistics.setAvgExamScore(0);
+            avgExamScore.ifPresent(value -> statistics.setAvgExamScore(
+                    (float) BigDecimal.valueOf(value).setScale(2, RoundingMode.HALF_UP).doubleValue()));
+        });
+
+        return statisticsList;
+    }
+}
